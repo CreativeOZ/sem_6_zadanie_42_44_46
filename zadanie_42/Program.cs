a7ab7e593fd93ea3d6ff8121c4a5d151c2ad69e0
CreativeOZ <zagriaoleg@gmail.com>
2022-03-11 20:01:12 +0300

    42. Определить сколько чисел больше 0 введено с клавиатуры

---
+++ zadanie_42/Program.cs
@@ -0,0 +1,15 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+Console.Clear();
+
+int[] array = new int[3];
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+   Console.Write("Ввод числа: ");
+   array[i] = int.Parse(Console.ReadLine());
+   if (array[i] > 0)
+   {
+       result = result + 1;
+   }
+}
+Console.WriteLine($"Колличество чисел больше 0 введено с клавиатуры = {result}");
