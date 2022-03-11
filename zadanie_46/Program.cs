c08446ee6fc1f2a89981f0215611889527187f0b
CreativeOZ <zagriaoleg@gmail.com>
2022-03-11 20:02:24 +0300

    46. Написать программу масштабирования фигуры

---
+++ zadanie_46/Program.cs
@@ -0,0 +1,15 @@
+﻿// 46. Написать программу масштабирования фигуры
+Console.Clear();
+
+string num = "(0,0) (2,0) (2,2) (0,2)";
+Console.Write("Введите множитель: ");
+int k = int.Parse(Console.ReadLine());
+num = num.Replace("(", "").Replace(")", "").Replace(",", " ");
+string[] array = num.Split(" "); 
+
+for (int i = 0; i < array.Length; i=i+2)
+{
+    int x = k * int.Parse(array[i]);
+    int y = k * int.Parse(array[i + 1]);
+    Console.Write($"({x},{y})");
+}
